5f3d5a668ac89bc695caed4f01b743ef9dc3c7ac
Wahyu Purnama <60025573+wahyupurnama43@users.noreply.github.com>
2024-06-19 18:04:11 +0800

    Feat : Update upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -37,36 +37,42 @@ function imageMultipleUpload(req, res, next) {
   const bucketName = process.env.BUCKETNAME;
 
   try {
-    
-    if (req.files.url_gambar || req.files.bukti_pembayaran) {
-      const url_gambar =  req.files.url_gambar[0];
-      const bukti_pembayaran =  req.files.bukti_pembayaran[0];
+    const user = req.loggedUser;
+    const bucket = storage.bucket(bucketName);
 
-      const user = req.loggedUser;
-      const bucket = storage.bucket(bucketName);
+    if (req.files.url_gambar) {
+      const url_gambar =  req.files.url_gambar[0];
 
       let imagePathUrl_gambar = '';
-      let imagePathBukti_pembayaran = '';
       let routePath = req.route.path;
       if (routePath === '/workshop' || routePath === '/workshop/:id') {
         imagePathUrl_gambar = `images/workshop/${user.id}_${nanoid(8)}_${
           url_gambar.originalname
         }`;
-        imagePathBukti_pembayaran = `images/buktiPembayaran/${user.id}_${nanoid(8)}_${
-          bukti_pembayaran.originalname
-        }`;
       }else if (routePath === '/workshop/extend' || routePath === '/workshop/extend/:id') {
         imagePathUrl_gambar = `images/workshop/update-${user.id}_${nanoid(8)}_${
           url_gambar.originalname
         }`;
+      }
+      bucket.file(imagePathUrl_gambar).save(url_gambar.buffer);
+      req.body.url_gambar = bucket.file(imagePathUrl_gambar).publicUrl();
+    }
+    
+    if(req.files.bukti_pembayaran){
+      const bukti_pembayaran =  req.files.bukti_pembayaran[0];
+
+      let imagePathBukti_pembayaran = '';
+      let routePath = req.route.path;
+      if (routePath === '/workshop' || routePath === '/workshop/:id') {
+        imagePathBukti_pembayaran = `images/buktiPembayaran/${user.id}_${nanoid(8)}_${
+          bukti_pembayaran.originalname
+        }`;
+      }else if (routePath === '/workshop/extend' || routePath === '/workshop/extend/:id') {
         imagePathBukti_pembayaran = `images/buktiPembayaran/update-${user.id}_${nanoid(8)}_${
           bukti_pembayaran.originalname
         }`;
       }
-      bucket.file(imagePathUrl_gambar).save(url_gambar.buffer);
       bucket.file(imagePathBukti_pembayaran).save(bukti_pembayaran.buffer);
-
-      req.body.url_gambar = bucket.file(imagePathUrl_gambar).publicUrl();
       req.body.bukti_pembayaran = bucket.file(imagePathBukti_pembayaran).publicUrl();
     }
     
@@ -76,6 +82,5 @@ function imageMultipleUpload(req, res, next) {
     next(error);
   }
 }
-
 
 module.exports = { imageUpload, imageMultipleUpload };
